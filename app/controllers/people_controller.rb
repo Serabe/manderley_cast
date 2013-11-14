cd42dece0f8dacb3eb255afbd513c5993ad1970c
Sergio Arbeo <serabe@gmail.com>
2013-11-14 16:16:51 +0100

    People is a root resource now

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,17 +1,23 @@
 class PeopleController < ApplicationController
-  before_action :set_person, except: [:create, :new]
-  before_action :set_movie
+  before_action :set_person, except: [:create, :new, :index]
+
+  def index
+    @people = Person.all
+  end
+
+  def show
+  end
 
   def new
-    @person = @movie.people.build
+    @person = Person.new
   end
 
   def create
-    @person = @movie.people.build person_params
+    @person = Person.new person_params
 
     respond_to do |format|
       if @person.save
-        format.html { redirect_to @movie, notice: 'Person was successfully added.' }
+        format.html { redirect_to @person, notice: 'Person was successfully added.' }
         format.json { render action: 'show', status: :created, location: @person }
       else
         format.html { render action: 'new' }
@@ -22,12 +28,15 @@ class PeopleController < ApplicationController
   end
 
   def destroy
-    @movie.destroy
+    @person.destroy
     
     respond_to do |format|
       format.html { redirect_to movie }
       format.json { head :no_content }
     end
+  end
+
+  def edit
   end
 
   def update
@@ -38,10 +47,6 @@ class PeopleController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_person
     @person = Person.find(params[:id])
-  end
-
-  def set_movie
-    @movie = Movie.find(params[:movie_id])
   end
  
   # Never trust parameters from the scary internet, only allow the white list through.
