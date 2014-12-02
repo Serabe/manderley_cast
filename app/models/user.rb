6740a85d0b8b7f7a2e76625cbd6597a9112002ec
Sergio Arbeo <serabe@gmail.com>
2014-12-02 10:02:18 +0100

    Add role to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  enum role: {  plain:  0,
+                admin:  1,
+                yoda:   2 } 
+
   has_many :comments
   validates :username, uniqueness: true
 end
