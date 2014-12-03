857f77898adfd9af1269d43f781520c8c8b89509
Sergio Arbeo <serabe@gmail.com>
2014-12-03 23:01:15 +0100

    JSON

--- app/assets/javascripts/kiss.js
+++ app/assets/javascripts/kiss.js
@@ -2,6 +2,7 @@ jQuery(function($) {
 
   $.get('/kiss/ajax').done(function(data) {
     var $json = $('#json');
+    console.log(data);
     $json.text(JSON.stringify(data));
   });
 });
---
+++ app/serializers/comment_serializer.rb
@@ -0,0 +1,13 @@
+class CommentSerializer < ActiveModel::Serializer
+  attributes :id, :body, :username
+
+  attribute :movie_title, key: :title
+
+  def username
+    object.user.username
+  end
+
+  def movie_title
+    object.movie.title
+  end
+end
